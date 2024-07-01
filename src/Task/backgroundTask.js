28ef43c2bd4e28629a80860598d55a26df0de5ba
unknown <gabrielquadro@gmail.com>
2024-06-30 22:24:27 -0300

    Create routine for post api each 1 minute

---
+++ src/Task/backgroundTask.js
@@ -0,0 +1,49 @@
+import * as TaskManager from 'expo-task-manager';
+import axios from 'axios';
+import * as SQLite from 'expo-sqlite';
+import React, { useState } from 'react';
+
+const db = SQLite.openDatabaseSync('leads.db');
+
+async function getListbyDb() {
+    console.log("Get info do sqlite");
+
+    try {
+        const query = 'SELECT * FROM leads';
+
+        const allRows = await db.getAllAsync(query)
+
+        console.log("Dados da tabela leads que serao enviados para api: ");
+
+        if(allRows.length <= 0){
+        console.log("Não há dados no sqlite no momento.");
+        }
+
+        for (const row of allRows) {
+            console.log(row.id, row.car_id, row.user_info);
+
+
+            axios.post('https://www.wswork.com.br/cars/leads', row)
+                .then(() => {
+                    db.runAsync('DELETE FROM leads WHERE id = $value', { $value: row.id });
+                    console.log("Linha enviada pra api excluida do sqlite com id = " + row.id)
+                })
+                .catch(error => console.error(error));
+
+        }
+
+        console.log("Fim da rotina em : " + new Date())
+
+    } catch (error) {
+        console.log(error)
+    }
+
+};
+
+TaskManager.defineTask('SEND_LEADS', () => {
+    console.log("----------------------------------------------------------------");
+    console.log("Realizando a rotina, inicio em: " + new Date());
+    getListbyDb();
+
+    return TaskManager.TaskManagerResult;
+});
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, {useEffect} from 'react';
 import Routes from "../routes"
+import * as BackgroundFetch from 'expo-background-fetch';
+import '../Task/backgroundTask';
 
 export default function Index() {
+
+    useEffect(() => {
+        const registerBackgroundTask = async () => {
+            try {
+                await BackgroundFetch.registerTaskAsync('SEND_LEADS', {
+                    minimumInterval: 60 * 1, // a cada 1 minuto
+                    stopOnTerminate: false,
+                    startOnBoot: true,
+                });
+            } catch (error) {
+                console.error('Erro ao registrar a tarefa!', error);
+            }
+        };
+    
+        registerBackgroundTask();
+    }, []);
+
     return (
         <Routes />
     )
